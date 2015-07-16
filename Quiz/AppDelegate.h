283181df28e64c8551b723a81076633b4dd45563
jamesreinke <james@rtsystems.io>
2015-07-16 15:26:43 -0700

    Initial Commit

---
+++ Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by James Reinke on 7/16/15.
+//  Copyright (c) 2015 Penguin Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Quiz/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by James Reinke on 7/16/15.
+//  Copyright (c) 2015 Penguin Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
